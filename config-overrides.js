dbd73bc063cb13314dc9b41f0608a2a4cf311e9f
Linh Pham <linhpt199@gmail.com>
2019-11-27 11:14:14 +0200

    Init project

---
+++ config-overrides.js
@@ -0,0 +1,15 @@
+import { override, fixBabelImports, addLessLoader } from 'customize-cra'
+
+export default override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true,
+  }),
+   addLessLoader({
+     javascriptEnabled: true,
+     modifyVars: {
+       '@primary-color': '#1890ff',
+     },
+   })
+ );
